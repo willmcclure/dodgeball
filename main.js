8c0585d6112704881510aa566c2cc1e166a70729
@willmcclure <willbmcclure@gmail.com>
2020-11-13 22:38:16 -0600

    completing checkpoint

--- main.js
+++ main.js
@@ -1,3 +1,15 @@
+// CODE PLAN
+ 
+//  Create two classes, one to represent a person and one a dodgeball player
+ 
+//  Transfer a person to being a dodgeball player then add that new player to an array of players. Allow users to select button to trasnfer that new dodgeball player to either red or blue team.
+ 
+// TESTING
+ 
+//  1) A new player object was created when a person is pulled from the arrOfPeople
+//  2) The new player was added to the listOfPlayers[]
+//  3) A new team member was created for both the red and blue teams
+
 const arrOfPeople = [
     {
       id: 2,
